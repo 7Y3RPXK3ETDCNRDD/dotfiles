a1ff5f4adddb4d16c9265aa6408a9fb51927c1f7
7Y3RPXK3ETDCNRDD <artka.kazaktar@gmail.com>
2015-12-27 15:29:57 +0600

    New configs

--- .i3/i3blocks/cmus_script.sh
+++ .i3/i3blocks/cmus_script.sh
@@ -1,9 +1,12 @@
+#!/usr/bin/bash
+
 ARTIST=$( cmus-remote -Q 2>/dev/null | grep 'tag artist' | cut -d " " -f 3- )
 TITLE=$( cmus-remote -Q 2>/dev/null | grep 'tag title' | cut -d " " -f 3- )
+NOTHING=$( cmus-remote -Q 2>/dev/null | grep 'file' | cut -d "/" -f 5- )
 
 if [ -z "$ARTIST" ];
 then
-echo "▶ Nothing"
+echo "  $NOTHING"
 else
-echo "▶ $ARTIST - $TITLE"
+echo "  $ARTIST - $TITLE"
 fi
