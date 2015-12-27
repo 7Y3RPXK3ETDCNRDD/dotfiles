27a13dac62bbea4c2147ff76c027e2664dacc2c4
7Y3RPXK3ETDCNRDD <artka.kazaktar@gmail.com>
2015-12-27 15:40:34 +0600

    Added fish config and date script

---
+++ fish/config.fish
@@ -0,0 +1,12 @@
+# Aliases
+alias g++ "g++ -std=c++11 -Wall -Wextra -g"
+alias clang++ "clang++ -std=c++11 -Wall -Wextra -g"
+
+# Vim keybindings
+fish_vi_key_bindings
+
+# Abbreviations
+abbr -a pacman sudo pacman -S
+abbr -a shutdown sudo shutdown now
+abbr -a reboot sudo reboot
+abbr -a size du -h --max-depth=1
